353f8b3b7a744a0111be9b1ab10b94c45f2d4119
LautaroJLZ <lautarojoseluis1234@gmail.com>
2024-09-08 19:31:24 -0300

    Actualizacion de Servicios y boton de consulta de precios

--- src/app/Servicios/page.jsx
+++ src/app/Servicios/page.jsx
@@ -1,25 +1,31 @@
 import Link from "next/link";
+// Iconos imports
+import { FaWhatsapp } from "react-icons/fa";
+// Iconos imports end
 
 const Servicios = () => {
   return (
     <div className="flex flex-wrap items-stretch gap-5 justify-center my-20 text-white">
       {/* Separador */}
-      <Link
-        href="/Contacto"
-        className="mt-2 md:mt-0 bg-BG-2 cursor-pointer hover:shadow-2xl hover:shadow-amarillo transition-all p-5 max-w-[400px] flex flex-col justify-between items-center rounded-md"
-      >
+      <div className="mt-2 md:mt-0 bg-BG-2 cursor-pointer hover:shadow-2xl hover:shadow-amarillo transition-all p-5 max-w-[400px] flex flex-col justify-between items-center rounded-md">
         <div>
           <div className="flex flex-col justify-center items-center p-2">
             <div className="flex flex-col justify-center items-center">
               <h3 className="text-amarillo font-bold text-2xl">Básico</h3>
             </div>
             <div className="flex space-x-2">
-              <p className="text-amarillo font-bold text-2xl">AR$</p>
-              <p className="text-amarillo font-bold text-2xl">7.000</p>
-              <p className="text-amarillo font-bold text-2xl">/mes</p>
+              <a
+                target="_blank"
+                href="https://wa.me/3755286624?text=Hola%20me%20interesa%20el%20plan%20de%20entrenamiento%20Básico"
+                className="flex justify-center items-center space-x-3 text-black font-bold bg-green-600 rounded-md p-2 my-1 hover:text-green-600 hover:bg-black transition-all duration-300"
+              >
+                <p>CONSULTAR PRE$IO</p>
+                <FaWhatsapp className="font-bold text-xl" />
+              </a>
             </div>
             <p className="text-center text-xs">Servicio Estandar</p>
           </div>
+          {/* Separador */}
           <ul class="w-full list-disc border-amarillo border p-2">
             <li className=" p-2 mt-2 ml-5">
               Entrenamiento guiado para que aprendas a entrenar por tu cuenta
@@ -27,10 +33,9 @@ const Servicios = () => {
             <li className="p-2 mt-2 ml-5">
               Vas a recibir una planilla mensual en donde se va a registrar:
               <ul class="list-disc ml-4">
-                <li className="p-1">Pesos</li>
-                <li className="p-1">Repeticiones</li>
-                <li className="p-1">Ejercicios</li>
-                <li className="p-1">Técnicas</li>
+                <li className="p-1">Registro del entrenamiento</li>
+                <li className="p-1">Registro del volumen del entrenamiento</li>
+                <li className="p-1">Registro de peso corporal</li>
               </ul>
             </li>
             <div className="w-full flex flex-col justify-center items-center my-5">
@@ -48,9 +53,9 @@ const Servicios = () => {
             />
           </div>
         </div>
-      </Link>
+      </div>
       {/* Separador */}
-      <Link
+      <div
         href="/Contacto"
         className="mt-2 md:mt-0 bg-BG-2 cursor-pointer hover:shadow-2xl hover:shadow-amarillo transition-all p-5 max-w-[400px] flex flex-col justify-between items-center rounded-md"
       >
@@ -62,23 +67,38 @@ const Servicios = () => {
               </h3>
             </div>
             <div className="flex space-x-2">
-              <p className="text-amarillo font-bold text-2xl">AR$</p>
-              <p className="text-amarillo font-bold text-2xl">12.000</p>
-              <p className="text-amarillo font-bold text-2xl">/mes</p>
+            <a
+                target="_blank"
+                href="https://wa.me/3755286624?text=Hola%20me%20interesa%20el%20plan%20de%20entrenamiento%20Personalizado"
+                className="flex justify-center items-center space-x-3 text-black font-bold bg-green-600 rounded-md p-2 my-1 hover:text-green-600 hover:bg-black transition-all duration-300"
+              >
+                <p>CONSULTAR PRE$IO</p>
+                <FaWhatsapp className="font-bold text-xl" />
+              </a>
             </div>
             <p className="text-center text-xs">Servicio Exclusivo</p>
           </div>
           <ul class="w-full list-disc border-amarillo border p-2">
             <li className=" p-2 mt-2 ml-5">
-              Entrenamiento y alimentacion personalizado.
+              Balanza digital inteligente.
             </li>
             <li className="p-2 mt-2 ml-5">
-              Entrenamiento guíado de intensidad, peso, técnica, seguridad,
-              revision, etc
+              Entrenamiento adaptado al cliente.
             </li>
             <li className="p-2 mt-2 ml-5">
-              Planilla y rutina planificada con métodos y técnicas adaptadas al
-              cliente
+              Alimentacion adaptada al cliente.
+            </li>
+            <li className="p-2 mt-2 ml-5">
+              Medidas corporales.
+            </li>
+            <li className="p-2 mt-2 ml-5">
+              Comprobación de fotos.
+            </li>
+            <li className="p-2 mt-2 ml-5">
+              Perdida de grasa (Periodización).
+            </li>
+            <li className="p-2 mt-2 ml-5">
+              Aumento de masa muscular (Periodización).
             </li>
             <div className="w-full flex flex-col justify-center items-center my-5">
               <p>Horarios</p>
@@ -95,9 +115,9 @@ const Servicios = () => {
             />
           </div>
         </div>
-      </Link>
+      </div>
       {/* Separador */}
-      <Link
+      <div
         href="/Contacto"
         className="mt-2 md:mt-0 bg-BG-2 cursor-pointer hover:shadow-2xl hover:shadow-amarillo transition-all p-5 max-w-[400px] flex flex-col justify-between items-center rounded-md"
       >
@@ -109,36 +129,41 @@ const Servicios = () => {
               </h3>
             </div>
             <div className="flex space-x-2">
-              <p className="text-amarillo font-bold text-2xl">AR$</p>
-              <p className="text-amarillo font-bold text-2xl">10.000</p>
-              <p className="text-amarillo font-bold text-2xl">/mes</p>
+            <a
+                target="_blank"
+                href="https://wa.me/3755286624?text=Hola%20me%20interesa%20el%20plan%20de%20entrenamiento%20Online"
+                className="flex justify-center items-center space-x-3 text-black font-bold bg-green-600 rounded-md p-2 my-1 hover:text-green-600 hover:bg-black transition-all duration-300"
+              >
+                <p>CONSULTAR PRE$IO</p>
+                <FaWhatsapp className="font-bold text-xl" />
+              </a>
             </div>
             <p className="text-center text-xs">Servicio Premium</p>
           </div>
           <ul class="w-full list-disc border-amarillo border p-2">
             <li className=" p-2 mt-2 ml-5">
-              Rutina de entrenamiento personalizado.
+              Perdida de grasa mes a mes.
             </li>
             <li className="p-2 mt-2 ml-5">
-              Plan de alimentación personalizado.
+              Aumento de masa muscular mes a mes.
             </li>
             <li className="p-2 mt-2 ml-5">
-              Control a travéz de fotos mensuales.
+              Conteo de calorías.
             </li>
             <li className="p-2 mt-2 ml-5">
-              Vas a recibir documentos PDF, Word, Vídeos y audios explicando al
-              detalle todo.
+              Guía nutricional.
+            </li>
+            <li className="p-2 mt-2 ml-5">
+              Entrenamiento eficaz y específico.
+            </li>
+            <li className="p-2 mt-2 ml-5">
+              Comprobación de fotos.
             </li>
             <div className="w-full flex justify-around items-center my-5">
               <div className="flex flex-col justify-center items-center">
                 <p>Horarios</p>
                 <p>Lunes a Domingo</p>
                 <p>24/7</p>
-              </div>
-              <div className="flex flex-col justify-center items-center">
-                <p>Reportes</p>
-                <p>Finalizando el mes</p>
-                <p>de pago</p>
               </div>
             </div>
           </ul>
@@ -151,7 +176,57 @@ const Servicios = () => {
             />
           </div>
         </div>
-      </Link>
+      </div>
+      {/* Separador */}
+      <div
+        href="/Contacto"
+        className="mt-2 md:mt-0 bg-BG-2 cursor-pointer hover:shadow-2xl hover:shadow-amarillo transition-all p-5 max-w-[400px] flex flex-col justify-between items-center rounded-md"
+      >
+        <div>
+          <div className="flex flex-col justify-center items-center p-2">
+            <div className="flex flex-col justify-center items-center">
+              <h3 className="text-amarillo font-bold text-2xl">
+                Control corporal
+              </h3>
+            </div>
+            <div className="flex space-x-2">
+            <a
+                target="_blank"
+                href="https://wa.me/3755662970?text=Hola%20me%20interesa%20el%20control%20corporal"
+                className="flex justify-center items-center space-x-3 text-black font-bold bg-green-600 rounded-md p-2 my-1 hover:text-green-600 hover:bg-black transition-all duration-300"
+              >
+                <p>CONSULTAR PRE$IO</p>
+                <FaWhatsapp className="font-bold text-xl" />
+              </a>
+            </div>
+          </div>
+          <ul class="w-full list-disc border-amarillo border p-2">
+            <li className=" p-2 mt-2 ml-5">
+              Toma de medidas corporales.
+            </li>
+            <li className="p-2 mt-2 ml-5">
+              Peso corporal.
+            </li>
+            <li className="p-2 mt-2 ml-5">
+              Medición de porcentaje % graso corporal.
+            </li>
+            <li className="p-2 mt-2 ml-5">
+            Medición de porcentaje % de masa muscular corporal.
+            </li>
+            <div className="">
+              <img src="/images/servicioControlCorporal.jpg" alt="FotoBalanza" />
+            </div>
+          </ul>
+
+          <div className="flex justify-center items-center my-5">
+            <img
+              src="/images/Logo.jpg"
+              alt="Logo"
+              className="w-[100px] h-[100px] rounded-full"
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
